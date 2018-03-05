53a8be25d4010134e32e4afc856841677672f715
micxye <michaelyeahh@gmail.com>
2018-03-05 12:37:11 -0800

    recursion stringifyJSON + getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,20 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  var result = [];
+  //compare className to className.children
+  var searchNode = function(node) {
+    if (node.classList.contains(className)) {
+        result.push(className);
+        //push true result to result array
+    }  
+    //iterate through all children until complete
+    for (var i = 0; i < node.children.length; i++) {
+      searchNode(node.children[i]);
+    }
+  }
+
+  searchNode(document.body);
+  //return result
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,51 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var resultStr = '';
+  
+  if (obj === undefined || typeof obj === 'function') {
+    return undefined;
+  }
+  if (typeof obj === 'number') {
+    resultStr += obj;
+  }
+  if (typeof obj === 'string') {
+    resultStr += '"' + obj + '"';
+  }
+  if (obj === null) {
+    resultStr += 'null'
+  }
+  if (typeof obj === 'boolean') {
+    if (obj === true) {
+      resultStr += 'true';
+    } else {
+      resultStr += 'false';
+    }
+  }
+  if (Array.isArray(obj)) {
+    var newStr = '[';
+    for (var i = 0; i < obj.length; i++) {
+      newStr += stringifyJSON(obj[i]);
+      if (i !== obj.length-1) {
+        newStr += ','
+      }
+    }
+    resultStr += newStr + ']';
+    
+  }
+  if (typeof obj === 'object' && obj !== null && !Array.isArray(obj)) {
+    var newStr = '{';
+    for (var key in obj) {
+      newStr += stringifyJSON(key) + ':' +stringifyJSON(obj[key]);
+      if (key !== Object.keys(obj)[Object.keys(obj).length - 1]) {
+        newStr += ','
+      }
+    };
+    resultStr += newStr + '}';
+  }
+  // stringifyJSON(obj)
+  if (resultStr.includes(undefined)) {
+    resultStr = undefined
+  }
+return resultStr;
 };
